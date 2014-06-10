24f8f926c35fdede3cc1a68937c889e2131fe3ef
Donovan Crichton <donovan.crichton@hushmail.com>
2014-06-10 21:27:30 +1000

    deleted old files

--- InClassExercise4/InClassExercise4.java
+++
@@ -1,7 +0,0 @@
-public class InClassExercise4 {
-	public static void main(String[] args) {
-		System.out.println("The number of minutes in in a year is calculated as"
-			+" follows:\n 60 (minutes) x 24 (hours) x 365 (days)."
-			+"\nresulting in: " + (60 * 24 * 365) + " minutes per year.");
-	}
-}     	
